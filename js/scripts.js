9683dd29fe342bff663bfb8cba83307698c8423d
BegoCaZa <begoxcz@hotmail.com>
2025-03-25 20:11:05 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -120,3 +120,141 @@ const doesThisWordHasAVowel=(words)=>{
      console.log(noVowelWords);
 }
 doesThisWordHasAVowel(['Agua', 'Pan', 'Elefante', 'Mesa', 'Oruga']);
+
+//8 - Bego tiene un array con números y debe devolver un nuevo array con solo los números impares multiplicados por 2.
+// Ejemplo entrada: [3, 6, 9, 12, 15]
+// Ejemplo salida: [6, 18, 30]
+
+const isThisNumberUneven =(numbers)=>{
+    const newArray=[];
+    for (const number of numbers) {
+        if(number%2 !==0){
+            newArray.push(number*2)
+        }
+    }
+    console.log(newArray);
+}
+isThisNumberUneven([3, 6, 9, 12, 15]);
+
+//9 - Sabrina quiere crear un sistema para clasificar números. Recibe un array y debe mostrar cuántos son pares, cuántos son impares y cuántos son cero.
+// Ejemplo entrada: [0, 3, 6, 9, 12, 0]
+// Ejemplo salida: Pares: 2 - Impares: 3 - Ceros: 2
+
+const numberClasificator=(numbers)=>{
+    let evenNumbers=0;
+    let unevenNumbers=0;
+    let zeros=0;
+
+    for (const number of numbers) {
+        if(number%2===0){
+            evenNumbers++;
+        } else if (number%1===0){
+            unevenNumbers++;
+        } else {
+            zeros++;
+        }
+    }
+    console.log(`Pares: ${evenNumbers} - Impares: ${unevenNumbers} - Ceros: ${zeros}`)
+}
+numberClasificator([0, 3, 6, 9, 12, 0]);
+
+//10 - Camila quiere un sistema que reciba un array de nombres y devuelva un array nuevo con las posiciones donde aparecen nombres con más de 5 letras.
+// Ejemplo entrada: ['Pedro', 'Ana', 'Esteban', 'María', 'Begoña']
+// Ejemplo salida: [2, 4]
+
+const  onlyNamesWithFourLetters=(names)=>{
+    let positionNamesWith4Letters=[];
+
+    for (let i = 0; i <= names.length; i++) {
+        if(names.length[i]===4){
+            positionNamesWith4Letters.push(i);
+        }        
+    }
+    console.log(positionNamesWith4Letters); 
+
+} //sale el array vacio
+onlyNamesWithFourLetters(['Pedro', 'Ana', 'Esteban', 'María', 'Begoña']);
+
+//11 - Macarena quiere generar un código de seguridad que se crea uniendo la primera letra de cada palabra en un array de frases.
+// Ejemplo entrada: ['Fuego en el bosque', 'Refugio seguro', 'Suministros esenciales']
+// Ejemplo salida: 'FRS'
+
+const codeGenerator=(words)=>{
+    let code="";
+
+    for (const word of words) {
+        code=code+word.charAt(0);
+    }
+
+    console.log(code);
+}
+codeGenerator(['Fuego en el bosque', 'Refugio seguro', 'Suministros esenciales']);
+
+//12 - Bego quiere hacer un sistema que reciba dos arrays de números y devuelva un array con los números que aparecen en ambos arrays.
+// Ejemplo entrada: [1, 2, 3, 4] y [3, 4, 5, 6]
+// Ejemplo salida: [3, 4]
+
+const whichNumberRepeats=()=>{
+    const numbers1=[1, 2, 3, 4];
+    const numbers2=[3, 4, 5, 6];
+    let repeatedNumbers=[];
+
+    for (let i = 0; i < numbers1.length; i++) {//debo revisar cada posicion y verificar si no se repite en el segundo array
+        if(numbers1.includes(numbers2[i])){ //ën este indice de numbers2, esta alguno de number1?
+            repeatedNumbers.push(numbers2[i]) //aaaah funciono con number 2, por?
+        }
+    }
+    console.log(repeatedNumbers);
+} 
+whichNumberRepeats([]);
+
+// 13 - Camila necesita generar un array que contenga todos los números entre dos números dados, inclusive.
+// Ejemplo entrada: 5 y 10
+// Ejemplo salida: [5, 6, 7, 8, 9, 10]
+
+const numbersInBetween=(numbers)=>{
+    let numbersBetween=[];
+    for (let i = numbers[0]; i <= numbers[1]; i++) { //como se que el array tiene 2 numeros solamente, le digo que empiece en 0 y termine en 1
+        numbersBetween.push(i);
+    }
+    console.log(numbersBetween);
+} //omg funcionoooo
+numbersInBetween([5,10]);
+
+//14 - Macarena quiere un programa que devuelva el número total de vocales en cada palabra de un array.
+// Ejemplo entrada: ['Hola', 'Mundo', 'JavaScript']
+// Ejemplo salida: [2, 2, 3]
+
+const howManyVowels=(words)=>{
+    const vowels="AEIOUaeiou";
+    let totalVowels=[];
+    let vowel=0; //contador de vocales
+
+    for (let i = 0; i < words.length; i++) {
+        // let vowel=0; //contador de vocales
+        for (const word of words) {
+            if (vowels.includes(word)){
+                vowel++;
+            }
+        }
+        totalVowels.push(vowel);
+        
+    }
+    console.log(totalVowels);
+} //no pude:(
+howManyVowels(['Hola', 'Mundo', 'JavaScript']);
+
+//15 - Bego quiere que cada string en un array se muestre al revés.
+// Ejemplo entrada: ['Hola', 'Mundo']
+// Ejemplo salida: ['aloH', 'odnuM']
+
+const reverseThisWord=(words)=>{
+    const reversedWords=[];
+
+    for (const word of words) {
+        const reversedWord= word.split("").reverse().join('');
+        reversedWords.push(reversedWord);
+    }
+    console.log(reversedWords);
+}
+reverseThisWord(['Hola', 'Mundo']);
